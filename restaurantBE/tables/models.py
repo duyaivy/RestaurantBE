61e339a50dfec8837d5c7bfa26009fe20199611b
duyaivy <quocduy0322@gmail.com>
2026-03-15 23:10:43 +0700

    fix: return moni category with get dish

--- restaurantBE/tables/models.py
+++ restaurantBE/tables/models.py
@@ -1,16 +1,20 @@
 from restaurantBE.constants.choices import TableStatus
 from django.db import models
 
+
 class Table(models.Model):
     number = models.IntegerField(primary_key=True)
     capacity = models.IntegerField(null=False)
-    status = models.CharField(max_length=20, choices=TableStatus.choices, default=TableStatus.AVAILABLE)
+    status = models.CharField(
+        max_length=20, choices=TableStatus.choices, default=TableStatus.AVAILABLE
+    )
     token = models.CharField(max_length=255, null=False)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "Table"
+        ordering = ["number"]
 
     def __str__(self):
         return f"Table {self.number}"
